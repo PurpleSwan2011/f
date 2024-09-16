59ba25358828b89c59bb83a08ab759bdbc4cfbf7
Saima Fathima <saima.fathi2011@gmail.com>
2024-09-16 21:05:41 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,21 @@
+name = "Saima"
+age = 13
+is_student = True
+weight = 26.7
+
+print("name :",name)
+print("datatype of name is ",type(name))
+print("age :",age)
+print("datatype of age is ",type(age))
+print("is_student :",is_student)
+print("datatype of is_student is ",type(is_student))
+print("weight :",weight)
+print("datatype of weight is ",type(weight))
+
+print("/n after type casting")
+age = str(age)
+print("age :",age)
+print("datatype of age is ",type(age))
+weight = int(weight)
+print("weight :",weight)
+print("datatype of weight is ",type(weight))
